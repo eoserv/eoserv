b0e7649ffb7bb719db3765b3a77c7e6efe289d26
Julian Smythe <sausage@tehsausage.com>
2009-03-08 11:40:24 +0000

    Uploaded missing eoconst.hpp
    git-svn-id: svn://svn.eoserv.net/eoserv/trunk@21 da1c5d5e-72c9-4536-8af9-af1c9134b0e6

---
+++ src/eoconst.hpp
@@ -0,0 +1,51 @@
+#ifndef EOCONST_HPP_INCLUDED
+#define EOCONST_HPP_INCLUDED
+
+const int ADMIN_PLAYER = 0;
+const int ADMIN_GUIDE = 1;
+const int ADMIN_GUARDIAN = 2;
+const int ADMIN_GM = 3;
+const int ADMIN_HGM = 4;
+
+const int DIRECTION_DOWN = 0;
+const int DIRECTION_LEFT = 1;
+const int DIRECTION_UP = 2;
+const int DIRECTION_RIGHT = 3;
+
+const int SIT_SITTING = 1;
+const int SIT_STANDING = 2;
+
+const int EMOTE_HAPPY = 1;
+const int EMOTE_DEPRESSED = 2;
+const int EMOTE_SAD = 3;
+const int EMOTE_ANGRY = 4;
+const int EMOTE_CONFUSED = 5;
+const int EMOTE_SURPRISED = 6;
+const int EMOTE_HEARTS = 7;
+const int EMOTE_MOON = 8;
+const int EMOTE_SUICIDAL = 9;
+const int EMOTE_EMBARASSED = 10;
+const int EMOTE_DRUNK = 11;
+const int EMOTE_TRACE = 12;
+const int EMOTE_LEVELUP = 13;
+const int EMOTE_PLAYFUL = 14;
+
+const int QUEST_PROGRESS = 1;
+const int QUEST_HISTORY = 2;
+
+const int FILE_MAP = 1;
+const int FILE_ITEM = 2;
+const int FILE_NPC = 3;
+const int FILE_SPELL = 4;
+const int FILE_CLASS = 5;
+
+const int SKIN_WHITE = 0;
+const int SKIN_YELLOW = 1;
+const int SKIN_TAN = 2;
+const int SKIN_PANDA = 3;
+const int SKIN_SKELETON = 4;
+const int SKIN_FISH = 5;
+
+
+
+#endif // EOCONST_HPP_INCLUDED
